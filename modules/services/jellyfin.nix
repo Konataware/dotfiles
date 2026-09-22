499a4e92589972d26f2aa0590567cf0ce85fc565
Konataware <luansiloe404@gmail.com>
2026-09-22 16:20:00 -0300

    update: jellyfin with shokofin and master systemd service
    Adding a shoko server container because of issues i had with organizing
    the Monogatari show. The graphroot is specified outside of / due to a
    partitioning quirk in my machine.
       - Master systemctl service for easier orchestration of Jellyfin
          - jellyfin-acl service is now inside the master service
          - shoko server container is initialized by the master service
          - jellyfin itself is also, initialized by the master service

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -1,13 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+
 {
   services.jellyfin = {
     enable = true;
-    openFirewall = true; # PORT DOCUMENTATION: (8096 for HTTP, 8920 for HTTPS, 1900 for DLNA)
+    openFirewall = true;
   };
+
+  systemd.services.jellyfin.wantedBy = lib.mkForce [ ];
+
   systemd.services.jellyfin-acl = {
     description = "jellyfin acl perms for ~/Media";
-    wantedBy = [ "multi-user.target" ];
+    wantedBy = [ "jellyfin.service" ];
     before = [ "jellyfin.service" ];
+    partOf = [ "jellyfin.service" ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
@@ -15,13 +20,50 @@
     script = ''
       ${pkgs.acl}/bin/setfacl -m u:jellyfin:--x /home/paz
       ${pkgs.acl}/bin/setfacl -R -m u:jellyfin:rX /home/paz/Media
-      ${pkgs.acl}/bin/setfacl -R -d -m u:jellyfin:rX /home/paz/Media
+      ${pkgs.findutils}/bin/find /home/paz/Media -type d \
+        -exec ${pkgs.acl}/bin/setfacl -d -m u:jellyfin:rX {} \;
     '';
+  };
+
+  virtualisation.containers.storage.settings = {
+    storage = {
+      driver = "overlay";
+      graphroot = "/home/paz/.local/share/containers/storage";
+      runroot = "/run/containers/storage";
+    };
+  };
+
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers.shoko = {
+      image = "ghcr.io/shokoanime/server:latest";
+      ports = [ "8111:8111" ];
+      volumes = [
+        "/home/paz/.shoko:/home/shoko/.shoko"
+        "/home/paz/Media:/mnt/anime:ro"
+      ];
+      environment = {
+        PUID = "1000";
+        PGID = "100";
+        TZ = "America/Sao_Paulo";
+      };
+      extraOptions = [ "--restart=unless-stopped" ];
+    };
+  };
+
+  services.logind.settings.Login = {
+    HandleLidSwitch = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+    HandleLidSwitchDocked = "ignore";
+    LidSwitchIgnoreInhibited = "no";
   };
 
   environment.systemPackages = with pkgs; [
     jellyfin
     jellyfin-web
     jellyfin-ffmpeg
+    podman
+    acl
+    findutils
   ];
 }
